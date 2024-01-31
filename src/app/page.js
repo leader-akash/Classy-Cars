794c5bde30adb10e72574b6a1d8d5a9e506328c4
leader-akash <leader.akashak@gmail.com>
2024-01-31 10:00:32 +0530

    fixed issues

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,3 @@
-import Image from "next/image";
-import Navbar from "./components/navbar/Navbar";
-import Banner from "./components/banner/Banner";
-import Card from "./components/cards/Card";
-import Footer from "./components/footer/Footer";
 import Homepage from "./pages/home/page";
 
 export default function Home() {
